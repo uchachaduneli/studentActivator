bef359ee71324d2b30853e884ddd903154ef9176
Ucha Chaduneli <uchachaduneli@gmail.com>
2021-03-02 14:28:36 +0400

    comment

---
+++ src/main/java/ge/bestline/studentActivator/StudentActivatorApplication.java
@@ -0,0 +1,13 @@
+package ge.bestline.studentActivator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentActivatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentActivatorApplication.class, args);
+	}
+
+}
