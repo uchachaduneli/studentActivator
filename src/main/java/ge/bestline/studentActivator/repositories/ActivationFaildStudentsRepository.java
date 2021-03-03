ed0d485e569bef6571e414975751b273eeb22909
Ucha Chaduneli <uchachaduneli@gmail.com>
2021-03-03 20:31:25 +0400

    removed old source

--- src/main/java/ge/bestline/studentActivator/repositories/ActivationFaildStudentsRepository.java
+++
@@ -1,7 +0,0 @@
-package ge.bestline.studentActivator.repositories;
-
-import ge.bestline.studentActivator.entities.ActivationFailedStudents;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ActivationFaildStudentsRepository extends CrudRepository<ActivationFailedStudents, Integer> {
-}
--- src/main/java/ge/bestline/studentActivator/repositories/ActivationJobConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package ge.bestline.studentActivator.repositories;
-
-import ge.bestline.studentActivator.entities.ActivationJobConfig;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ActivationJobConfigRepository extends CrudRepository<ActivationJobConfig, Integer> {
-}
